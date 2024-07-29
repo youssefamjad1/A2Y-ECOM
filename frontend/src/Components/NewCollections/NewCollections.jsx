406f2da2f55a097eb39851a0cd569f9f3b9e51e2
Youssef HIMMI <yhimmi1@gmail.com>
2024-07-29 21:53:55 +0200

    Update frontend configuration and code

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,29 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import './NewCollections.css'
-import Item from '../Item/Item'
+import React, { useEffect, useState } from 'react';
+import './NewCollections.css';
+import Item from '../Item/Item';
+
+// Define the API URL constant
+const API_URL = 'https://a2y-ecom-1.onrender.com';
 
 const NewCollections = () => {
+  const [new_collections, setNew_collections] = useState([]);
 
-  const[new_collections, setNew_collections]=useState([]);
-  useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
-    .then((response)=>response.json())
-    .then((data)=>setNew_collections(data));
-  },[])
+  useEffect(() => {
+    fetch(`${API_URL}/newcollections`)
+      .then((response) => response.json())
+      .then((data) => setNew_collections(data))
+      .catch((error) => console.error('Error fetching new collections:', error));
+  }, []);
 
   return (
     <div className='new-collections'>
-       <h1>NEW COLLECTIONS</h1>
-       <hr />
-       <div className="collections">
-        {new_collections.map((item, i)=>{
-          console.log(new_collections);
-          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-        })}
-  
-       </div>
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {new_collections.map((item, i) => (
+          <Item 
+            key={i} 
+            id={item.id} 
+            name={item.name} 
+            image={item.image} 
+            new_price={item.new_price} 
+            old_price={item.old_price} 
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewCollections
+export default NewCollections;
--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -1,30 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import './Popular.css'
-import Item from '../Item/Item'
+import React, { useEffect, useState } from 'react';
+import './Popular.css';
+import Item from '../Item/Item';
+
+// Define the API URL constant
+const API_URL = 'https://a2y-ecom-1.onrender.com';
 
 const Popular = () => {
-const[popularProducts, setPopularProducts]=useState([]);
+  const [popularProducts, setPopularProducts] = useState([]);
 
-useEffect(()=>{
-  fetch('http://localhost:4000/popularinwomen')
-  .then((response)=>response.json())
-  .then((data)=>setPopularProducts(data));
-
-},[]);
+  useEffect(() => {
+    fetch(`${API_URL}/popularinwomen`)
+      .then((response) => response.json())
+      .then((data) => setPopularProducts(data))
+      .catch((error) => console.error('Error fetching popular products:', error));
+  }, []);
 
   return (
     <div className='popular'>
-       <h1>POPULAR IN WOMEN</h1>
-       <hr />
-       <div className="popular-item">
-        {popularProducts.map((item, i)=>{
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-
-        })}
-       </div>
-
+      <h1>POPULAR IN WOMEN</h1>
+      <hr />
+      <div className="popular-item">
+        {popularProducts.map((item, i) => (
+          <Item 
+            key={i} 
+            id={item.id} 
+            name={item.name} 
+            image={item.image} 
+            new_price={item.new_price} 
+            old_price={item.old_price} 
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Popular
+export default Popular;
